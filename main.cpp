dba26bf6b23b1a47265f6a73e63da26d41648f94
reneselander <50113423+reneselander@users.noreply.github.com>
2021-08-18 19:42:01 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+    // funktion: menu
+
+void showMenu() {
+
+    cout << "* * * * * MENU * * * * *" << endl;
+
+    cout << "" << endl;
+
+    cout << "     1 Check balance" << endl;
+
+    cout << "" << endl;
+
+    cout << "     2 Deposit" << endl;
+
+    cout << "" << endl;
+
+    cout << "     3 Withdraw" << endl;
+
+    cout << "" << endl;
+
+    cout << "     4 E X I T" << endl;
+
+    cout << "" << endl;
+
+    cout << "* * * * * * * * * * * *" << endl;
+
+    cout << "" << endl;
+    
+
+}
+
+int main()
+
+{
+
+    // check balance, deposit, withdraw, show menu
+
+    // variabel
+
+    int option;
+
+    double balance = 540;
+
+    // aktivera funktionen som visar menyn
+
+    // starta loopen
+
+    do
+
+    {
+        
+    showMenu();
+
+    cout << "Option: ";
+
+    cin >> option;
+
+    if(!(option < 5 && option > 0)) {
+        showMenu();
+        //cout << "end";
+       //break;
+       return 0;
+    }
+        
+    //system("cls");
+
+    switch (option)
+    {
+
+    case 1: cout << "Balance is: " << balance << " Euro" << endl;
+
+    cout << "" << endl;
+    
+        break;
+
+    case 2: cout << "Deposit amount: ";
+
+        double depositAmount;
+
+        cin >> depositAmount;
+
+        balance += depositAmount;
+
+        break;
+
+    case 3: cout << "Withdraw amount: ";
+
+        double withdrawAmount;
+
+        cin >> withdrawAmount;
+
+        if (withdrawAmount<=balance)
+
+            balance -= withdrawAmount;
+
+        else
+
+            cout << "Not enough money" << endl;
+
+        break;        
+    
+    }
+
+    }
+    
+
+    while (option!=4);    
+
+    //system("pause>0");
+    return 0;
+}
